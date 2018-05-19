d5ad0054edc38cb1acb8be894357e3e2f86642dd
fragrantpie <fragrantpie@gmail.com>
2018-05-19 05:08:29 -0600

    First Commit

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("Welcome to ETCH-a-Sketch!")
+
+// var container = document.getElementById("container");
+// 
+// // Create the squares.
+// let square = document.createElement("DIV");
+// square.setAttribute("style", "background-color: pink; height: 60px; width: 60px;");
+// 
+// container.appendChild(square);
+
